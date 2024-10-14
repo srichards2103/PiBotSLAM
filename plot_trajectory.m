4925a441fe495080f30be106334050aeff0d7ff9
nnyffynn <fynn.s.penny@gmail.com>
2024-10-14 12:56:43 +1100

    Made plot_trajectory.m more efficient by limiting the number of scatter plots for landmarks.

--- plot_trajectory.m
+++ plot_trajectory.m
@@ -1,4 +1,6 @@
 function plot_trajectory(vis_data)
+    persistent plotted_landmarks
+    
     figure;
     hold on;
     
@@ -6,6 +8,14 @@ function plot_trajectory(vis_data)
     robot_positions = cat(2, vis_data.robot_pos);
     num_positions = size(robot_positions, 2);
     colors = jet(num_positions); % Create a color map
+    
+    % Get the current number of landmarks
+    current_num_landmarks = size(vis_data(1).landmark_pos, 2);
+    
+    % Initialize or resize plotted_landmarks if necessary
+    if isempty(plotted_landmarks) || length(plotted_landmarks) ~= current_num_landmarks
+        plotted_landmarks = zeros(1, current_num_landmarks);
+    end
     
     for i = 1:num_positions
         % Get position uncertainty (assuming it's stored in vis_data.robot_cov)
@@ -19,21 +29,34 @@ function plot_trajectory(vis_data)
         scatter(robot_positions(1,i), robot_positions(2,i), circle_size, colors(i,:), 'filled', 'MarkerFaceAlpha', 0.5);
         
         % Plot landmark estimates for this time step
-        % landmark_pos = vis_data(i).landmark_pos;
-        % landmark_cov = vis_data(i).landmark_cov;
-        % num_landmarks = size(landmark_pos, 2);
+        landmark_pos = vis_data(i).landmark_pos;
+        landmark_cov = vis_data(i).landmark_cov;
+        num_landmarks = size(landmark_pos, 2);
         
-        % for j = 1:num_landmarks
-        %     % Get landmark uncertainty
-        %     lm_cov = landmark_cov(2*j-1:2*j, 2*j-1:2*j);
-        %     lm_uncertainty = sqrt(trace(lm_cov));
+        for j = 1:num_landmarks
+            % Ensure plotted_landmarks has an entry for this landmark
+            if j > length(plotted_landmarks)
+                plotted_landmarks(j) = 0;
+            end
             
-        %     % Scale marker size based on uncertainty
-        %     lm_size = 20 + 100 * lm_uncertainty;
+            % Check if we've already plotted this landmark a few times
+            if plotted_landmarks(j) >= 10
+                continue;  % Skip this landmark if we've already plotted it 10 times
+            end
             
-        %     % Plot landmark estimate with size based on uncertainty
-        %     scatter(landmark_pos(1,j), landmark_pos(2,j), lm_size, 'b*', 'MarkerEdgeAlpha', 0.5);
-        % end
+            % Get landmark uncertainty
+            lm_cov = landmark_cov(2*j-1:2*j, 2*j-1:2*j);
+            lm_uncertainty = sqrt(trace(lm_cov));
+            
+            % Scale marker size based on uncertainty
+            lm_size = 20 + 100 * lm_uncertainty;
+            
+            % Plot landmark estimate with size based on uncertainty
+            scatter(landmark_pos(1,j), landmark_pos(2,j), lm_size, 'b*', 'MarkerEdgeAlpha', 0.5);
+            
+            % Increment the count for this landmark
+            plotted_landmarks(j) = plotted_landmarks(j) + 1;
+        end
     end
     
     % Plot final landmark positions with landmark numbers
